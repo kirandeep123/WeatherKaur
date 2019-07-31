819016386ecf3f30fcaa272494b748c5250f228b
kirandeep123 <me@kiran.cc>
2019-07-31 19:09:40 +0530

    Updates

---
+++ precache-manifest.9f05cae20d9c1d501bcbf30121f3c905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf0b96cc8d791975da2133b1d06f0da",
+    "url": "/WeatherKaur/index.html"
+  },
+  {
+    "revision": "e6445ab44c70e1c55869",
+    "url": "/WeatherKaur/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "7dc7011c1937271d4b6c",
+    "url": "/WeatherKaur/static/css/main.b1ab0e2a.chunk.css"
+  },
+  {
+    "revision": "e6445ab44c70e1c55869",
+    "url": "/WeatherKaur/static/js/2.f1394722.chunk.js"
+  },
+  {
+    "revision": "7dc7011c1937271d4b6c",
+    "url": "/WeatherKaur/static/js/main.f97845c6.chunk.js"
+  },
+  {
+    "revision": "fd0415149b21cc3b1a78",
+    "url": "/WeatherKaur/static/js/runtime~main.9c7c5ce3.js"
+  }
+]);
